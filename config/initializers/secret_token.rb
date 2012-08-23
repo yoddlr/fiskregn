87d806583bf5754bdfd49392e772808b7d8b817d
Snorvarg <jonlennryd@yahoo.se>
2012-08-23 15:35:11 +0200

    installed rails, rspec, cucumber and other sensible defaults.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fiskregn::Application.config.secret_token = '4d1444fdd0937b49c6baeaabe253b769a51541ff997bfe3b739a835f313c11667c9159ab1716a6b9840c508f46075d16fe0ab8f57415ba37814f2de4c9cc085d'
