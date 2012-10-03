2d70f8e8cf386dd42906e80d56c684c5df979553
Hampus Ahlm Brenander <hmmps@skenkonst.se>
2012-10-03 12:15:13 +0200

    add test for validation of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  
+  validates :email, presence: true
+  validates :password, confirmation: true, length: {minimum: 6}, format: {with: /.*(^[A-z]+.*[0-9]+.*$)|(^[0-9]+.*[A-z]+.*$)/}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,33 @@ describe User do
   it "has a valid factory" do
     expect(build :user).to be_valid
   end
+  
+  describe 'validations' do
+    
+    before :all do
+      @user = build :user
+    end
+    
+    it "requires an email" do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+    
+    describe 'a valid password' do
+      it "has at least 6 characters" do
+        build(:user, password: '123ab').should_not be_valid
+      end
+    
+      it "has at least 1 digit" do
+        build(:user, password: 'abcdef%').should_not be_valid
+      end
+    
+      it "has at least 1 letter" do
+        build(:user, password: '123456.').should_not be_valid
+      end
+      it "can contain symbols, letters and numbers" do
+        expect(build(:user, password: 'xc#4g%sd', password_confirmation: 'xc#4g%sd')).to be_valid
+      end
+    end
+  end
 end
