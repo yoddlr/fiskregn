cc45bd81c3db579bd8d44b0ecc5e09e0b38bc226
Hampus Ahlm Brenander <hmmps@skenkonst.se>
2012-10-11 12:35:56 +0200

    Fix compiling and caching for asset pipeline for dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,9 @@ Fiskregn::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  
+  config.assets.compile = true
+  config.assets.digest = true
 
   # Expands the lines which load the assets
   config.assets.debug = true
