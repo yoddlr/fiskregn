0b14763537301978efd3e081b642f4d6c6aa784f
Hampus Ahlm Brenander <hmmps@skenkonst.se>
2012-10-15 15:22:23 +0200

    Create associations parent/children for content

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,8 @@
 class Content < ActiveRecord::Base
-  attr_accessible :data, :user_id, :parent_id
+  attr_accessible :data, :user_id, :parent
   
-  belongs_to :user
+  belongs_to :user 
+
+  belongs_to :parent, :class_name => 'Content' 
+  has_many :children, :class_name => 'Content', :foreign_key => :parent_id
 end
