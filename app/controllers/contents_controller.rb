c62eb2bfdf9d2899e60b5dbe90e3568c1ddd21ec
Per Gärdén <per.garden@lexiconitkonsult.se>
2012-10-22 09:50:26 +0200

    Allowing contents without location. Removing hack from 89ea2da84d1cf57d2646b9f0899e52c41a93310d

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,49 +3,40 @@ class ContentsController < ApplicationController
   # rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
   
   def new
-    # Is there an attempted target location?
-    if params[:location]
+    # Must be signed in to create contents
+    if current_user
       # Attempted target location, defaulting to current user's location
       @location_id = params[:location] || current_user.location.id
-      if current_user
-        @content = Content.new
-        @content.parent_id = params[:parent_id] if params[:parent_id]
-      else
-        # Back to attempted target location if not signed in
-        redirect_to user_path(User.find(Location.find(@location_id).user_id)), notice: I18n.t('.must_be_signed_in_to_create_contents')
-      end
+      @content = Content.new
+      @content.parent_id = params[:parent_id] if params[:parent_id]
+      @content.add_location(Location.find(@location_id))
+    elsif params[:location]
+      # Back to attempted target location if not signed in
+      redirect_to user_path(User.find(Location.find(params[:location]).user_id)), notice: I18n.t('.must_be_signed_in_to_create_contents')
     else
-      # Unforeseen nirvana: Calling new_content_path without location
       redirect_to root_path, notice: I18n.t('.must_be_signed_in_to_create_contents')
     end
   end
   
   def create
-    # Psychopathically changing param name from location to location_id :-|
-    if params[:location_id]
+    if current_user # params[:location_id]
       # Attempted target location, defaulting to current user's location
+      # Psychopathically changing param name from location to location_id :-|
       @location_id = params[:location_id] || current_user.location.id
-      if current_user
-        @content = Content.new(params[:content])
-        @content.user = current_user
-        @content.add_location(Location.find(@location_id))
-        # @content.parent = Content.find(params[:content][:parent_id]) if params[:content][:parent_id]
-        if @content.save
-          if @content.parent
-            # Saving content as a reply
-            redirect_to content_path(@content.parent), notice: I18n.t('.content_saved')
-          else
-            redirect_to user_path(Location.find(@location_id).user_id), notice: I18n.t('.content_saved')
-          end
+      @content = Content.new(params[:content])
+      @content.user = current_user
+      @content.add_location(Location.find(@location_id))
+      if @content.save
+        if @content.parent
+          # Saving content as a reply
+          redirect_to content_path(@content.parent), notice: I18n.t('.content_saved')
         else
-          render_template :new, alert: I18n.t('.content_not_saved')
+          redirect_to user_path(Location.find(@location_id).user_id), notice: I18n.t('.content_saved')
         end
       else
-        redirect_to home_index_path, notice: I18n.t('.content_not_saved')
-        # render_template :new, alert: I18n.t('.content_not_saved')
+        render_template :new, alert: I18n.t('.content_not_saved')
       end
     else
-      # Unforeseen nirvana: Calling create_content_path without location
       redirect_to root_path, notice: I18n.t('.must_be_signed_in_to_create_contents')
     end
   end
