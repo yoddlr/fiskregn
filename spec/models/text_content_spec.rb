12cbe49da8984ffc37d916ba1367840d856d623c
Per Gärdén <per.garden@lexiconitkonsult.se>
2012-10-26 14:35:15 +0200

    Starting specking of (text)content tagging. Hm... as this is generic for all and any kinds of content, should it be in a content-spec after all?

--- spec/models/text_content_spec.rb
+++ spec/models/text_content_spec.rb
@@ -9,4 +9,17 @@ describe TextContent do
   describe "Location" do
     pending
   end
+
+  describe 'Tag list' do
+    it "has a tag list" do
+      @untagged = create :text_content
+      expect(@untagged.tag_list.empty?).to be true
+    end
+    it "it accepts tags" do
+      @tagged = create :text_content
+      @tag = 'Taggety-tag'
+      @tagged.tag_list << @tag
+      expect(@tagged.tag_list.include?(@tag)).to be true
+    end
+  end
 end
