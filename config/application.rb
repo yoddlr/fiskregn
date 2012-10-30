0989bf468fb535246aed4a560e42f4e56f056dcb
Snorvarg <jonlennryd@yahoo.se>
2012-10-30 15:58:33 +0100

    Added Spanish. :)

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,8 @@ module Fiskregn
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.available_locales = [:en, :de, 'sv-SE']
+    # Devise translations available here: https://github.com/plataformatec/devise/wiki/I18n
+    config.i18n.available_locales = [:en, :de, 'sv-SE', :es]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
