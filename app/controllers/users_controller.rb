d2b7f878ec6c4c954f23d87a0046bd5d9799bf05
Per Gärdén <per.garden@lexiconitkonsult.se>
2012-11-01 12:09:02 +0100

    Cleansing lot's of commented out stuff.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,10 @@ class UsersController < ApplicationController
   def show
     # what the user shows to other users and guest
     @user = User.find(params[:id])
-    #@contents = @user.contents.tagged_with(["troll","snor"], :any => true )
-    #@contents = Content.tagged_with(["troll","snor"], :any => true )
-    #@contents = @user.owned_taggings
-    #@contents = Content.tagged_with( ["apon", "snor"], :any => true, :owned_by => @user.id)
     @contents = @user.contents
   end
   
   def home
-    #@contents = @user.contents.tagged_with(["troll","snor"], :any => true )
-    #@contents = Content.tagged_with(["troll","snor"], :any => true )
-    #@contents = Content.tagged_with("troll" , :owned_by => current_user)
     @contents = current_user.contents
   end
 end
