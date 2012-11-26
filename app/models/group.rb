3ff09646972ae022dac26235c28b2826eb7dd54f
Per Gärdén <per.garden@lexiconitkonsult.se>
2012-11-26 13:11:37 +0100

    Group name shall exist and be unique

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,8 @@
 class Group < ActiveRecord::Base
   attr_accessible :name
 
+  validates :name, presence: true, uniqueness: true
+
   has_and_belongs_to_many :admins, 
     join_table: "admins_groups",
     class_name: "User",
